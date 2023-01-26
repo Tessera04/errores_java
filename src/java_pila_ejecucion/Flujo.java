f643fd6deac8536cd427de7df1bb69990fca545f
Matias <matute_11@hotmail.com.ar>
2023-01-26 03:41:03 -0300

    modificanco try y catch

--- src/java_pila_ejecucion/Flujo.java
+++ src/java_pila_ejecucion/Flujo.java
@@ -17,10 +17,21 @@ public class Flujo {
 		System.out.println("Inicio metodo2");
 		for(int i = 1; i <= 5; i++) {
 	        System.out.println(i);
+	        
 	        try{
-	            int a = i / 0;
-	        } catch(ArithmeticException ex) {
-	            System.out.println("ArithmeticException");
+	            if (i == 3) {
+	            	int num = 0;
+	            	int resultado = i/num;
+	            	System.out.println(resultado);
+	            }
+	            
+	            String test = null;
+	            System.out.println(test.toString());
+	            
+	        } catch(ArithmeticException | NullPointerException exception) {
+	            System.out.println("Atrapo Exception");
+	            System.out.println(exception.getMessage());
+	            exception.printStackTrace();
 	        }
 		}
 		System.out.println("Fin metodo2");
