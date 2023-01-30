3365759a6b6d8678502911f4b03b08923a699dcb
Matias <matute_11@hotmail.com.ar>
2023-01-30 13:55:02 -0300

    Aplicando finally

--- src/java_pila_ejecucion/testConexion.java
+++ src/java_pila_ejecucion/testConexion.java
@@ -2,11 +2,11 @@ package java_pila_ejecucion;
 
 public class testConexion {
 	public static void main(String[] args) {
-		Conexion con = new Conexion();
+		Conexion con = null;
 		try {
+			con = new Conexion();
 			con.leerDatos();
 		}catch (IllegalStateException ex){
-			System.out.println("Recibiendo exception");
 			ex.printStackTrace();
 		} finally {
 			con.cerrar();
